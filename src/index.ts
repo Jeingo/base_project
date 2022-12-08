edd8c0eff2d976e9b6bb3c71258d619a02a11f5e
Renat <jeingo20@gmail.com>
2022-12-08 21:43:59 +0300

    add !!!

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: any, res: any) => {
-    let helloWorld = 'Hello World!';
+    let helloWorld = 'Hello World!!!';
     res.send(helloWorld)
 })
 
